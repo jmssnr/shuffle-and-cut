7c7f0f8b45dd60a08e5eb0fd1d90ff7698324947
jmssnr <maussner.johannes@gmail.com>
2023-08-27 10:32:55 +0200

    adds Mantine component library

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,31 @@
-import type { AppProps } from 'next/app'
+import { AppProps } from "next/app";
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>
+          shuffl | Modelling, Simulation & Analysis of Card Shuffles
+        </title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: "light",
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
