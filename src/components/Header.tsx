4fac50acbbcaa4b41e840838145d5337c1497d76
jmssnr <maussner.johannes@gmail.com>
2023-08-27 18:22:44 +0200

    adds simulation page; adds page header

---
+++ src/components/Header.tsx
@@ -0,0 +1,32 @@
+import { Paper, Group, Anchor, ActionIcon } from "@mantine/core";
+import { useRouter } from "next/router";
+import { IconBrandGithub, IconBrandInstagram } from "@tabler/icons-react";
+export const Header = () => {
+  const router = useRouter();
+
+  return (
+    <Paper p="xs" style={{ height: 50 }} shadow="sm">
+      <Group align="center" position="apart">
+        <Anchor
+          underline={false}
+          variant="gradient"
+          gradient={{ from: "indigo", to: "blue", deg: 45 }}
+          sx={{ fontFamily: "Greycliff CF, sans-serif" }}
+          fz="xl"
+          fw={700}
+          onClick={() => router.push("/")}
+        >
+          shuffl.
+        </Anchor>
+        <Group>
+          <ActionIcon>
+            <IconBrandGithub size="1.1rem" stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon>
+            <IconBrandInstagram size="1.1rem" stroke={1.5} />
+          </ActionIcon>
+        </Group>
+      </Group>
+    </Paper>
+  );
+};
